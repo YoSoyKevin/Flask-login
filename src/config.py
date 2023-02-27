602ee66b2f1a3e4151fae7e88a87797a47802cdd
YoSoyKevin <kevinshimizu1@gmai.com>
2023-02-26 21:17:58 -0500

    Conexion a la base de datos

--- src/config.py
+++ src/config.py
@@ -1,5 +1,12 @@
-class DevelopmentConfig():
+class Config:
+    SECRET_KEY='abecedarioCONññññ1123'
+
+class DevelopmentConfig(Config):
     DEBUG = True
+    MYSQL_HOST='localhost'
+    MYSQL_USER='root'
+    MYSQL_PASSWORD='1234'
+    MYSQL_DB='flask_login'
 
 config= {
     'development': DevelopmentConfig
